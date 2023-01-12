aa4e81110f9c18273c4d2d4f40b3d974c2ea5f45
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2023-01-12 13:19:20 +0300

    add Blocks list component

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,5 +1,11 @@
-const MainPage: React.FC = () => {
-  return <div></div>;
+import BlocksList from "../components/blocks-list/blocks-list";
+
+function MainPage(): JSX.Element {
+  return (
+    <div>
+      <BlocksList />
+    </div>
+  );
 };
 
 export default MainPage;
