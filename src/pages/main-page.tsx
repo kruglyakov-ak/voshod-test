1f26fa001bb8aa32245669ba5e2c651a00d2b6fa
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2023-01-12 15:00:29 +0300

    add logo

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,8 +1,10 @@
 import BlocksList from "../components/blocks-list/blocks-list";
+import logo from "../assets/img/logo.svg"
 
 function MainPage(): JSX.Element {
   return (
     <div>
+      <img src={logo} alt="logo" />
       <BlocksList />
     </div>
   );
