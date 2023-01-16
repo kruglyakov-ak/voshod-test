f0174d9d140ffba60a523c870a1ea75b419f1241
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2023-01-16 16:29:46 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./app";
 import { setupStore } from "./store/store";
 import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
