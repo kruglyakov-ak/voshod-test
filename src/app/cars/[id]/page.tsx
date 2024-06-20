421d421850d20aa1332b6c7daa8dd5fc23bde694
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2024-06-21 01:03:24 +0300

    get all static params

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -3,15 +3,27 @@ import { CarsListDtoSchema } from "@/entities/cars/api";
 import { z } from "zod";
 
 export async function generateStaticParams() {
-  const res = await fetch('https://test.taxivoshod.ru/api/test/?w=catalog-cars');
+  const res = await fetch(
+    "https://test.taxivoshod.ru/api/test/?w=catalog-cars",
+  );
   const cars: z.infer<typeof CarsListDtoSchema> = await res.json();
-  CarsListDtoSchema.parse(cars)
- 
+  CarsListDtoSchema.parse(cars);
+
+  new Array(cars.pages || 1 - 1).forEach(async (_, index) => {
+    const r = await fetch(
+      `https://test.taxivoshod.ru/api/test/?w=catalog-cars&page=${index + 1}`,
+    );
+    const c: z.infer<typeof CarsListDtoSchema> = await r.json();
+    CarsListDtoSchema.parse(c);
+
+    cars.list.push(...c.list);
+  });
+
   return cars.list.map((car) => ({
     id: String(car.id),
-  }))
+  }));
 }
 
 export default function Car() {
-  return <CarPage  />;
+  return <CarPage />;
 }
