f63b61ba9aceacebba0ac9b5ce7eb198b524d172
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2024-06-21 00:24:51 +0300

    fix: change car page for SSG

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -1,9 +1,9 @@
 import { CarPage } from "@/entities/car/components/car-page";
 
-export default function Car({ params }: { params: { id: string } }) {
-  return (
-    <main className="flex min-h-screen flex-col items-center">
-      <CarPage id={params.id} />
-    </main>
-  );
+export async function generateStaticParams() {
+  return [{ id: "fallback" }];
+}
+
+export default function Car() {
+  return <CarPage  />;
 }
--- src/entities/car/components/car-page.tsx
+++ src/entities/car/components/car-page.tsx
@@ -21,70 +21,74 @@ import {
 } from "@/shared/ui/carousel";
 import Image from "next/image";
 
-export function CarPage({ id }: { id: string }) {
-  const { data, isLoading, isFetching } = carApi.useGetCarQuery({ id });
+import { useParams } from "next/navigation";
+export function CarPage() {
+  const { id } = useParams();
+  const { data, isLoading, isFetching } = carApi.useGetCarQuery({ id: id as string });
   const router = useRouter();
 
   return (
-    <div className="w-[100vw] h-[100vh] p-5 flex justify-center items-center">
-      {isLoading && isFetching ? (
-        <Spinner size={"large"} className="scale-10" />
-      ) : (
-        <>
-          <Card className={"min-h-max min-w-[500px] p-5"}>
-            <Button onClick={router.back}>Назад</Button>
-            <CardHeader className="px-0">
-              <CardTitle className="w-max ">
-                {data?.item.brand} {data?.item.model}
-              </CardTitle>
-              <CardDescription>
-                <>Цена: {data?.item.price}</>
-                <br />
-                {!!data?.item.tarif.length && (
-                  <>Тариф: {data?.item.tarif.join(", ")}</>
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="w-[100vw] h-[100vh] p-5 flex justify-center items-center">
+        {isLoading && isFetching ? (
+          <Spinner size={"large"} className="scale-10" />
+        ) : (
+          <>
+            <Card className={"min-h-max min-w-[500px] p-5"}>
+              <Button onClick={router.back}>Назад</Button>
+              <CardHeader className="px-0">
+                <CardTitle className="w-max ">
+                  {data?.item.brand} {data?.item.model}
+                </CardTitle>
+                <CardDescription>
+                  <>Цена: {data?.item.price}</>
+                  <br />
+                  {!!data?.item.tarif.length && (
+                    <>Тариф: {data?.item.tarif.join(", ")}</>
+                  )}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="px-0 w-full flex items-center justify-center">
+                {data?.item?.images ? (
+                  <Carousel
+                    className="w-full max-w-sm"
+                    opts={{
+                      align: "start",
+                      loop: true,
+                    }}
+                  >
+                    <CarouselContent className="-ml-1">
+                      {data?.item?.images.map(({ image, id }) => (
+                        <CarouselItem
+                          key={id}
+                          className="pl-1 md:basis-1/2 lg:basis-1/3"
+                        >
+                          <div className="p-1">
+                            <Card>
+                              <CardContent className="flex aspect-square items-center justify-center p-6 relative">
+                                <Image
+                                  src={image}
+                                  alt={`${data?.item.brand + data?.item.model}`}
+                                  fill
+                                  loader={() => image}
+                                />
+                              </CardContent>
+                            </Card>
+                          </div>
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                  </Carousel>
+                ) : (
+                  <p>Нет изображений</p>
                 )}
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="px-0 w-full flex items-center justify-center">
-              {data?.item?.images ? (
-                <Carousel
-                  className="w-full max-w-sm"
-                  opts={{
-                    align: "start",
-                    loop: true,
-                  }}
-                >
-                  <CarouselContent className="-ml-1">
-                    {data?.item?.images.map(({ image, id }) => (
-                      <CarouselItem
-                        key={id}
-                        className="pl-1 md:basis-1/2 lg:basis-1/3"
-                      >
-                        <div className="p-1">
-                          <Card>
-                            <CardContent className="flex aspect-square items-center justify-center p-6 relative">
-                              <Image
-                                src={image}
-                                alt={`${data?.item.brand + data?.item.model}`}
-                                fill
-                                loader={() => image}
-                              />
-                            </CardContent>
-                          </Card>
-                        </div>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious />
-                  <CarouselNext />
-                </Carousel>
-              ) : (
-                <p>Нет изображений</p>
-              )}
-            </CardContent>
-          </Card>
-        </>
-      )}
-    </div>
+              </CardContent>
+            </Card>
+          </>
+        )}
+      </div>
+    </main>
   );
 }
